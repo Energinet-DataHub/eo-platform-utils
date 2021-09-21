d414267c4ed108793c9f14a8155bd898b668f895
Jakob Kristensen <jakob@jakobk.dk>
2021-09-21 09:56:14 +0200

    Under development

--- tests/integrationtest/api/test_context.py
+++ tests/integrationtest/api/test_context.py
@@ -2,7 +2,6 @@ from flask.testing import FlaskClient
 
 from energytt_platform.api import Application
 from energytt_platform.models.auth import InternalToken
-from energytt_platform.tokens import TokenEncoder
 
 from .endpoints import (
     EndpointRequiresContextReturnsToken,
@@ -14,7 +13,7 @@ class TestContext:
     TODO
     """
 
-    def test__provide_no_token__should_return_null(
+    def test__no_token_provided__should_return_null(
             self,
             app: Application,
             client: FlaskClient,
@@ -40,12 +39,10 @@ class TestContext:
         assert r.status_code == 200
         assert r.json == {'token': None}
 
-    def test__provide_valid_token__should_return_token_as_json(
+    def test__invalid_token_provided__should_return_null(
             self,
             app: Application,
             client: FlaskClient,
-            valid_token: InternalToken,
-            token_encoder: TokenEncoder[InternalToken],
     ):
         """
         TODO
@@ -59,13 +56,42 @@ class TestContext:
             endpoint=EndpointRequiresContextReturnsToken(),
         )
 
-        token_encoded = token_encoder.encode(valid_token)
+        # -- Act -------------------------------------------------------------
+
+        r = client.post(
+            path='/something',
+            headers={'Authorization': f'Bearer: INVALID-TOKEN'},
+        )
+
+        # -- Assert ----------------------------------------------------------
+
+        assert r.status_code == 200
+        assert r.json == {'token': None}
+
+    def test__valid_token_provided__should_return_token_as_json(
+            self,
+            app: Application,
+            client: FlaskClient,
+            valid_token: InternalToken,
+            valid_token_encoded: str,
+    ):
+        """
+        TODO
+        """
+
+        # -- Arrange ---------------------------------------------------------
+
+        app.add_endpoint(
+            method='POST',
+            path='/something',
+            endpoint=EndpointRequiresContextReturnsToken(),
+        )
 
         # -- Act -------------------------------------------------------------
 
         r = client.post(
             path='/something',
-            headers={'Authorization': f'Bearer: {token_encoded}'},
+            headers={'Authorization': f'Bearer: {valid_token_encoded}'},
         )
 
         # -- Assert ----------------------------------------------------------
