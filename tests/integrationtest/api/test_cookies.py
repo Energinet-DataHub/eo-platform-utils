48857a848adcc3aac5ae8d8da27961faead6f6aa
Jakob Kristensen <jakob@jakobk.dk>
2021-09-23 10:46:47 +0200

    Under development

--- tests/integrationtest/api/test_cookies.py
+++ tests/integrationtest/api/test_cookies.py
@@ -1,79 +1,13 @@
 import pytest
 from datetime import datetime
 from itertools import product
-from wsgiref.headers import Headers
 from flask.testing import FlaskClient
-from http.cookies import SimpleCookie
-from typing import Optional, Dict, Iterable, Any
+from typing import Dict, Iterable, Any
 
+from energytt_platform.api.testing import CookieTester
 from energytt_platform.api import Application, HttpResponse, Cookie
 
 from .endpoints import EndpointReturnsGeneric
-
-
-class CookieTester(object):
-    """
-    Helper for testing cookies in HTTP responses.
-    """
-    def __init__(self, headers: Headers):
-        """
-        :param headers:
-        """
-        self.cookies = SimpleCookie('\r\n'.join(headers.get_all('Set-Cookie')))
-
-    def assert_has_cookies(self, *names: str) -> 'CookieTester':
-        """
-        Assert that exactly the specified cookies were set.
-
-        :param names:
-        """
-        assert tuple(self.cookies.keys()) == names
-
-        return self
-
-    def assert_cookie(
-            self,
-            name: str,
-            value: str,
-            expires: Optional[datetime] = None,
-            path: Optional[str] = None,
-            comment: Optional[str] = None,
-            domain: Optional[str] = None,
-            max_age: Optional[str] = None,
-            secure: Optional[bool] = None,
-            http_only: Optional[bool] = None,
-            version: Optional[str] = None,
-            same_site: Optional[bool] = None,
-    ) -> 'CookieTester':
-        """
-        Assert content of a cookie.
-
-        :param name:
-        :param value:
-        :param expires:
-        :param path:
-        :param comment:
-        :param domain:
-        :param max_age:
-        :param secure:
-        :param http_only:
-        :param version:
-        :param same_site:
-        :return:
-        """
-        assert self.cookies[name].value == value
-        assert self.cookies[name]['expires'] == \
-               (expires.strftime('%a, %d %b %Y %H:%M:%S GMT') if expires is not None else '')
-        assert self.cookies[name]['path'] == (path if path is not None else '')
-        assert self.cookies[name]['comment'] == (comment if comment is not None else '')
-        assert self.cookies[name]['domain'] == (domain if domain is not None else '')
-        assert self.cookies[name]['max-age'] == (max_age if max_age is not None else '')
-        assert self.cookies[name]['secure'] == (secure if secure else '')
-        assert self.cookies[name]['httponly'] == (http_only if http_only else '')
-        assert self.cookies[name]['version'] == (version if version is not None else '')
-        assert self.cookies[name]['samesite'] == ('Strict' if same_site else '')
-
-        return self
 
 
 def get_cookie_combinations() -> Iterable[Dict[str, Any]]:
