b915e0ce320ee8926d555adcc5e1abb856c2dec4
Jakob Kristensen <jakob@jakobk.dk>
2021-10-18 15:39:48 +0200

    Code comments

--- energytt_platform/encrypt.py
+++ energytt_platform/encrypt.py
@@ -1,13 +1,10 @@
-# AES 256 encryption/decryption using pycrypto library
-
-import base64
-import hashlib
 from Crypto import Random
 from Crypto.Cipher import AES
+from hashlib import sha256
+from base64 import b64encode, b64decode
+
 
 _BLOCK_SIZE = 16
-# _pad = lambda s: s + (_BLOCK_SIZE - len(s) % _BLOCK_SIZE) * chr(_BLOCK_SIZE - len(s) % _BLOCK_SIZE)
-# _unpad = lambda s: s[:-ord(s[len(s) - 1:])]
 
 
 def _pad(s: str) -> str:
@@ -21,23 +18,31 @@ def _unpad(s: bytes) -> bytes:
 
 def aes256_encrypt(data: str, key: str) -> str:
     """
-    TODO
+    AES 256-encrypts a string.
+
+    :param data: The data to encrypt
+    :param key: Encryption key
+    :returns: Encrypted data (base64 encoded)
     """
-    private_key = hashlib.sha256(key.encode('utf8')).digest()
+    private_key = sha256(key.encode('utf8')).digest()
     data = _pad(data)
     iv = Random.new().read(AES.block_size)
     cipher = AES.new(private_key, AES.MODE_CBC, iv)
-    return base64.b64encode(
+    return b64encode(
         iv + cipher.encrypt(data.encode('utf8'))).decode('utf8')
 
 
 def aes256_decrypt(data_encrypted: str, key: str) -> str:
     """
-    TODO
+    AES 256-decrypts a string.
+
+    :param data_encrypted: The encrypted data (base64 encoded)
+    :param key: Encryption key
+    :returns: Decrypted data
     """
     data_encrypted = data_encrypted.encode('utf8')
-    private_key = hashlib.sha256(key.encode('utf8')).digest()
-    data_encrypted = base64.b64decode(data_encrypted)
+    private_key = sha256(key.encode('utf8')).digest()
+    data_encrypted = b64decode(data_encrypted)
     iv = data_encrypted[:16]
     cipher = AES.new(private_key, AES.MODE_CBC, iv)
     return _unpad(cipher.decrypt(data_encrypted[16:])).decode('utf8')
