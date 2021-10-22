03d502ba7fb29e3f5601b42088d86364f3c72603
Jakob Kristensen <jakob@jakobk.dk>
2021-10-22 09:01:17 +0200

    Added pycryptodome to dependencies in setup.py

--- setup.py
+++ setup.py
@@ -117,6 +117,7 @@ setuptools.setup(
         'sqlalchemy',
         'alembic',
         'requests',
+        'pycryptodome',
     ],
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
