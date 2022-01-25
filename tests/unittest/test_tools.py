596603ff49e0fa041897d216ddf7ca02269d48eb
Chris Bue Hansen <chrisbuehansen@gmail.com>
2022-01-25 11:10:29 +0100

    Changed append_query_parameters to also include path. Added tests to make sure the refactor didn't break anything

---
+++ tests/unittest/test_tools.py
@@ -0,0 +1,50 @@
+import urllib.parse as urlparse
+
+from origin.tools import url_append
+
+
+def test__url_append__add_query_params__should_format_url_correctly():  # noqa: E501
+
+    # -- Arrange -------------------------------------------------------------
+
+    url = 'https://bacon.com/spam?foo=bar&bacon=spam'
+
+    # -- Act -----------------------------------------------------------------
+
+    return_url = url_append(
+        url=url,
+        query_extra={
+            'foo': 'test',
+            'cake': 'brownie',
+        },
+    )
+
+    # -- Assert --------------------------------------------------------------
+
+    return_url_parts = list(urlparse.urlparse(return_url))
+    return_query = dict(urlparse.parse_qsl(return_url_parts[4]))
+
+    assert return_url[:23] == 'https://bacon.com/spam?'
+    assert return_query == {
+        'foo': 'test',
+        'cake': 'brownie',
+        'bacon': 'spam',
+    }
+
+
+def test__url_append__add_path__should_format_url_correctly():
+
+    # -- Arrange -------------------------------------------------------------
+
+    url = 'https://bacon.com/spam?foo=bar&bacon=spam'
+
+    # -- Act -----------------------------------------------------------------
+
+    return_url = url_append(
+        url=url,
+        path_extra='/cake'
+    )
+
+    # -- Assert --------------------------------------------------------------
+
+    assert return_url[:28] == 'https://bacon.com/spam/cake?'
