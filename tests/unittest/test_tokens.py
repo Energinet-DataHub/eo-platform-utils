2d5f9a6e44ea42c9a0b964b06a7ae9e0441e1906
rasmus godske <rasmusgodske@gmail.com>
2022-02-15 12:48:42 +0000

    added test

--- tests/unittest/test_tokens.py
+++ tests/unittest/test_tokens.py
@@ -1,5 +1,8 @@
 from dataclasses import dataclass
 
+import jwt
+import pytest
+
 from origin.bus import Message
 from origin.tokens import TokenEncoder
 
@@ -41,3 +44,44 @@ class TestTokenEncoder:
         assert isinstance(decoded, Message1)
         assert decoded != encoded
         assert decoded == obj
+
+    def test__tamper_with_token_payload__should_fail_to_decode(self):
+
+        # -- Arrange ---------------------------------------------------------
+
+        obj = Message1(
+            something='something',
+            nested=Nested(something='something nested'),
+        )
+
+        uut = TokenEncoder(
+            schema=Message1,
+            secret='123',
+            alg=TokenEncoder.HS256,
+        )
+
+        # -- Act -------------------------------------------------------------
+
+        encoded = uut.encode(obj=obj)
+
+        # Decode token
+        raw_decoded_token = jwt.decode(
+            encoded, "secret",
+            algorithms=[TokenEncoder.HS256],
+            options={"verify_signature": False}
+        )
+
+        # Update payload
+        raw_decoded_token['something'] = 'something else'
+
+        # Reencrypt token with invalid secret
+        new_encoded = jwt.encode(
+            raw_decoded_token,
+            "invalid-secret-key",
+            algorithm="HS256",
+        )
+
+        # -- Assert ----------------------------------------------------------
+
+        with pytest.raises(TokenEncoder.DecodeError):
+            uut.decode(encoded_jwt=new_encoded)
